f183c74ade9404e61bf210d7040aa186ce06a443
jwarzocha <warjed@gmail.com>
2014-07-30 10:07:28 +0200

    create table and insert

--- PraktykiMySQL3_okienko/src/Okno.java
+++ PraktykiMySQL3_okienko/src/Okno.java
@@ -71,22 +71,26 @@ public class Okno extends JFrame
 	        setDefaultCloseOperation(EXIT_ON_CLOSE);
 	    }
 	     
+	    
+	    //importuj button
 	    private class ImportujButtonHandler implements ActionListener
 	    {
 	        public void actionPerformed(ActionEvent e) 
 	        {
+	        	//pobieranie nazwy pliku
 	            nazwaPliku = textfieldPoleTekstowe.getText();
+	            
+	            //begin pobieranie danych z pliku
 	            String str="";String str2="";
-	            String[] rekord = new String[100000];
+	            String[] rekord = new String[1000000];
 	            for(int i=0;i<rekord.length;i++) rekord[i]=new String();
 	            int iloscKolumn=0,iloscWhile=0,iloscRekordow=0;
 	            CSVReader reader;
 				try {
 					reader = new CSVReader(new FileReader(nazwaPliku));
-				
 		    	    String[] row;
-		    	   
-		    	    try {
+		    	    try 
+		    	    {
 						while ((row = reader.readNext()) != null) 
 						{
 							for (int i = 0; i < row.length; i++) 
@@ -94,34 +98,26 @@ public class Okno extends JFrame
 								if(iloscWhile==0) iloscKolumn++;
 								rekord[iloscRekordow]=row[i];
 								//System.out.print(iloscRekordow+" "+rekord[iloscRekordow]+"; ");
-								
 								iloscRekordow++;
 						    }
 							iloscWhile++;
 							System.out.println();
 						    //break;
 						}
-						//System.out.print(iloscKolumn);
-						//firmybudownictwo.csv
-						
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-				} catch (FileNotFoundException e3) {
-					// TODO Auto-generated catch block
-					e3.printStackTrace();
-				}
-	            
+					}catch(IOException e1) {e1.printStackTrace();}
+				}catch(FileNotFoundException e3) {e3.printStackTrace();}
+				//end pobieranie danych z pliku
+				
 
-	    //---------------WRZUCANIE DANYCH DO BAZY DANYCH----------------------------------------------	
-	    	    
+//---------------WRZUCANIE DANYCH DO BAZY DANYCH----------------------------------------------	
+    	    
 	    		Connection conn = null;
 	    		Statement stmt = null;
 	    		try
 	    		{
 	    			//Register JDBC driver
 	    			Class.forName("com.mysql.jdbc.Driver");
+	    			
 	    			//Open a connection
 	    			conn = DriverManager.getConnection(DB_URL,USER,PASS);
 	    			System.out.println("Connection to database succeed.");
@@ -131,100 +127,51 @@ public class Okno extends JFrame
 	    			stmt = conn.createStatement();
 	    			String sql="";
 	    			
+	    			//nazwa pliku
 	    			String nazwaTabeli= nazwaPliku.substring(0, nazwaPliku.length()-4);
-// niedziała	drop    			
-//	    			sql="DROP TABLE "+nazwaPliku;
-//	    			stmt.executeUpdate(sql);
-//	    			System.out.println("Table  deleted in given database...");
-	    			
 
 	    			
-//	    			sql = "CREATE TABLE "+nazwaTabeli+" (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, ";
-//	    	        for(int i=0;i<iloscKolumn;i++){
-//	    	        	rekord[i]=rekord[i].replaceAll(" ", "_");
-//	    	        	if(i<iloscKolumn-1)sql=sql+rekord[i]+" VARCHAR(300), ";
-//	    	        	if(i==iloscKolumn-1)sql=sql+rekord[i]+" VARCHAR(300));";
-//	    	        }
-//	    	        System.out.println(sql);
-//	    			stmt.executeUpdate(sql);
+	    			//begin create table
+	    			System.out.println(iloscKolumn);
+	    			sql = "CREATE TABLE "+nazwaTabeli+" (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, ";
+	    	        for(int i=0;i<iloscKolumn;i++){
+	    	        	rekord[i]=rekord[i].replaceAll(" ", "_");
+	    	        	if((i<iloscKolumn-1))sql=sql+rekord[i]+" VARCHAR(300), ";
+	    	        	if(i==iloscKolumn-1)sql=sql+rekord[i]+" VARCHAR(300));";
+	    	        }
+	    	        System.out.println(sql);
+	    			stmt.executeUpdate(sql);
+	    			//end create table
 	    			
 	    			//begin insert
 	    			sql = "INSERT INTO "+nazwaTabeli+" (";
-	    			//firmy (nazwa_firmy, wojewodztwo, miejscowosc, ulica, kod_pocztowy, osoba_kontaktowa, telefon, tel_kom, adres_www, nip, regon, zatrudnienie) VALUES ";               
 	    			
-	    			 for(int i=0;i<iloscKolumn;i++){
-		    	        	rekord[i]=rekord[i].replaceAll(" ", "_");
-		    	        	if(i<iloscKolumn-1)sql=sql+rekord[i]+", ";
-		    	        	if(i==iloscKolumn-1)sql=sql+rekord[i]+") VALUES ('";
-		    	     }
+	    			for(int i=0;i<iloscKolumn;i++){
+		    	       	rekord[i]=rekord[i].replaceAll(" ", "_");
+		    	       	if(i<iloscKolumn-1)sql=sql+rekord[i]+", ";
+		    	       	if(i==iloscKolumn-1)sql=sql+rekord[i]+") VALUES ('";
+		    	    }
 	    			System.out.println(sql);
-
-	    			for(int i=iloscKolumn;i<iloscRekordow;i++)
+	    			int j;
+	    			for(int i=iloscKolumn;i<iloscRekordow-1;i++)
 	    			{
-	    				if((i%iloscKolumn!=0)){//&(i==iloscKolumn)){
-	    					str=str+rekord[i]+"','";System.out.println("cos");
+	    				for(j=0;j<iloscKolumn;j++){
+		    				if(j!=iloscKolumn-1){
+		    					str=str+rekord[i+j]+"','";
+		    				}
+		    				if(j==iloscKolumn-1){
+			    	        	str=str+rekord[i+j]+"');";
+			    	        	str2=sql+str;
+			    	        	stmt.executeUpdate(str2);
+			    	        	System.out.println(i+"\t"+str2);
+			    	        	str2="";str="";
+			    	        }
 	    				}
-	    				else if((i%iloscKolumn==0)){
-		    	        	str=str+rekord[i]+"');";
-		    	        	str2=sql+str;
-		    	        	//stmt.executeUpdate(str2);
-		    	        	System.out.println(i+"\t"+str2);
-		    	        	str2="";str="";
-		    	        }
-	    				
+	    				i=i+j;
 	    			}
-	    			
-	    			//end insert
-	    			
-	    			
-//	    			//begin select
-//	    			sql = "SELECT * FROM firmy";
-//	    			ResultSet rs = stmt.executeQuery(sql);
-//	    
-//	    			
-//	    			//Extract data from result set
-//	    			while(rs.next())
-//	    			{
-//	    				//Retrieve by column name
-//	    				int id  = rs.getInt("id");
-//	    				String nazwa_firmy = rs.getString("nazwa_firmy");
-//	    				String wojewodztwo = rs.getString("wojewodztwo");
-//	    				String miejscowosc = rs.getString("miejscowosc");
-//	    				String ulica = rs.getString("ulica");
-//	    				String kod_pocztowy = rs.getString("kod_pocztowy");
-//	    				String osoba_kontaktowa = rs.getString("osoba_kontaktowa");
-//	    				String telefon = rs.getString("telefon");
-//	    				String tel_kom = rs.getString("tel_kom");
-//	    				String adres_www = rs.getString("adres_www");
-//	    				String nip = rs.getString("nip");
-//	    				String regon = rs.getString("regon");
-//	    				String zatrudnienie = rs.getString("zatrudnienie");
-//	    
-//	    				//Display values
-//	    				
-//	    				
-//	    				System.out.print(id);
-//	    				System.out.print("\t" + nazwa_firmy);
-//	    				System.out.print("\t" + wojewodztwo);
-//	    				System.out.print("\t" + miejscowosc);
-//	    				System.out.print("\t" + ulica);
-//	    				System.out.print("\t" + kod_pocztowy);
-//	    				System.out.print("\t" + osoba_kontaktowa);
-//	    				System.out.print("\t" + telefon);
-//	    				System.out.print("\t" + tel_kom);
-//	    				System.out.print("\t" + adres_www);
-//	    				System.out.print("\t" + nip);
-//	    				System.out.print("\t" + regon);
-//	    				System.out.println("\t" + zatrudnienie);
-//	    
-//	    			
-//	    			
-//	    			
-//	    			}//end while 
-//	    			//end select
-//	    			
-//	    			//Clean-up environment
-//	    			rs.close();
+	    			//end insert	
+
+	    			//Clean-up environment
 	    			stmt.close();
 	    			conn.close();
 	    		}
@@ -236,17 +183,15 @@ public class Okno extends JFrame
 	    			try{if(conn!=null) conn.close();}catch(SQLException se){se.printStackTrace();}//end try        
 	    		}//end finally
 	    	   
-	    		
-	    	   
-	    	   
-	    	   System.out.println("kk");     
+	    		//napis końcowy
+	    		System.out.println("kk");     
 	        }
 	    }
 	     
 	    
 	    
 	    
-	    //wyjscie
+	    //wyjscie button
 	    public class wyjdzButtonHandler implements ActionListener{
 	        public void actionPerformed(ActionEvent e){
 	            System.exit(0);
